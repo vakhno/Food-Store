da9a10e2ab6c666d02915c7f7d2f9373226a8eb1
vakhno <vakhnohere@gmail.com>
2020-12-30 23:44:56 +0200

    Added Dishes component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, {useState, useEffect} from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import {Main, Dishes, Contacts} from './pages'
 
 function App() {
-	
+	const [dishes, setDishes] = useState([])
+
+	useEffect(() => {
+		fetch('http://localhost:3000/Food-Store/db.json')
+		.then((resp) => resp.json())
+		.then(json => setDishes(json.pizzas))
+	}, [])
+
+	console.log(dishes)
+
 	return (
 		<Router basename="/Food-Store">
 			<Header/>
 				<Route path ='/m' exact component={Main} />
-				<Route path ='/' exact component={Dishes} />
+				<Route path ='/' exact render={() => <Dishes items={dishes}/>} />
 				<Route path ='/contacts' component={Contacts} />
 			<Footer/>
 		</Router>
