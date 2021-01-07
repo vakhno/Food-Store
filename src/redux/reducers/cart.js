79301fd174c95398554c7b2a80e82f7113c0d75b
vakhno <vakhnohere@gmail.com>
2021-01-07 04:32:34 +0200

    Finished button-cart info

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,21 +6,22 @@ const initialState = {
 
 const cart = (state = initialState, action) => {
 	switch(action.type){
-		case 'ADD_DISH_CART':
-			return {
-				...state,
-				items:{
+		case 'ADD_DISH_CART': {
+			const newItems = {
 					...state.items,
 					[action.payload.id]: !state.items[action.payload.id] 
 					? [action.payload] 
 					: [...state.items[action.payload.id], action.payload, ] 
-					}
 			}
-		// case 'SET_TOTAL_COUNT':
-		// 	return {
-		// 		...state,
-		// 		totalCount: action.payload
-		// 	}
+
+			return {
+				...state,
+				items: newItems,
+				// totalCount: [].concat.apply([], Object.values(items)).length,
+				totalCount: Object.values(newItems).reduce( (accum, current) => accum.concat(current), []).length,
+				totalPrice: Object.values(newItems).reduce( (accum, current) =>  current.reduce( (accum, current) => current.price + accum, 0) + accum, 0)  
+			}
+		}
 		default:
 			return state
 	}
