7a3d9f1aab9e5710e240f9732992ea41de8c16bd
vakhno <vakhnohere@gmail.com>
2021-01-10 04:25:01 +0200

    Added working array cart sorting (NEED REFACTORING)

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,26 +3,18 @@ const initialState = {
 	totalPrice: 0,
 	totalCount: 0,
 }
-// elem.type === payload.type && elem.size === payload.size
+
 const placeNewDish = (state, payload) => {
-	console.log(state.items[payload.id])
-	console.log(payload)
-	console.log(Object.values(state.items[payload.id]).filter( elem => elem.some(elem => elem.type === payload.type && elem.size === payload.size)))
-	
 	let lengthDishesTypes = Object.keys(state.items[payload.id]).length
-	let	sameTypeIndex = Object.values(state.items[payload.id]).map( (elem, index) => elem[0].type === payload.type && elem[0].size === payload.size ? index : null)
+	let	sameTypeIndex = Object.values(state.items[payload.id]).findIndex( elem => elem[0].type === payload.type && elem[0].size === payload.size)
 
-	if(sameTypeIndex){
-		console.log(1)
-		state.items[payload.id][`${payload.id}.${++sameTypeIndex[0]}`].push(payload)
-		return state.items[payload.id]
+	if(sameTypeIndex !== -1){
+		state.items[payload.id][`${payload.id}.${++sameTypeIndex}`].push(payload)
 	}else{
-		console.log(2)
-		return [...state.items[payload.id], payload]
-
+		state.items[payload.id][`${payload.id}.${++lengthDishesTypes}`] = [payload]
 	}
 
-	// return [...state.items[payload.id], payload]
+	return state.items[payload.id]
 }
 
 const cart = (state = initialState, action) => {
@@ -34,17 +26,10 @@ const cart = (state = initialState, action) => {
 					? {[action.payload.id+0.1]: [action.payload]} 
 					: placeNewDish(state, action.payload) 
 			}
-			console.log('---------------------')
 			console.log(newItems)
-			// console.log(newItems['1'])
-			// console.log(newItems['1']['1.1'])
-
 			return {
 				...state,
 				items: newItems,
-				// totalCount: [].concat.apply([], Object.values(items)).length,
-				// totalCount: Object.values(newItems).reduce( (accum, current) => accum.concat(current), []).length,
-				// totalPrice: Object.values(newItems).reduce( (accum, current) =>  current.reduce( (accum, current) => current.price + accum, 0) + accum, 0)  
 			}
 		}
 		default:
